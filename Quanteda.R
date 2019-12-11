bf4bdec7b7e98fabbc5da01f66c58e41f4d58000
Gulfon <1155096864@link.cuhk.edu.hk>
2019-12-11 12:47:03 +0800

    Added Chinese Demo

--- Quanteda.R
+++ Quanteda.R
@@ -1,23 +1,41 @@
 #Chainsaw Stuff ----
-install.packages(c('geometry', 'Rtsne', 'rsvd'))
-install.packages(c('tidyverse', 'rio', 'quanteda', 'rtweet', 'topicmodels', 'stm'))
-install.packages("parsedate")
-library(parsedate)
-install.packages(c('spacyr', 'igraph'))
-install.packages("psych")
-library("psych")
-install.packages("desc")
-library(dplyr)
-library(descr)
-require(spacyr)
-library(quanteda)
-spacy_install()
+install.packages('installr')
+library(installr)
+install.Rtools()
+
+install.packages(c('geometry', 'Rtsne', 'rsvd','spacyr',
+                   'igraph','tidyverse', 'rio', 'quanteda',
+                   'rtweet', 'topicmodels', 'stm','parsedate',
+                   'psych','desc','zoo','lubridate','newsmap','dplyr','descr','readtext','ggplot2'
+  ))
+
 devtools::install_github("quanteda/quanteda.corpora")
-install.packages("newsmap")
-require(quanteda)
-require(readtext)
-require(quanteda.corpora)
-require(newsmap)
+
+library(dplyr)
+library(desc)
+library(quanteda)
+library(geometry)
+library(readtext)
+library(quanteda.corpora)
+library(rsvd)
+library(spacyr)
+library(igraph)
+library(ggplot2)
+library(tidyverse)
+library(lubridate)
+library(newsmap)
+library(descr)
+library(psych)
+library(parsedate)
+library(stm)
+library(Rtsne)
+library(rio)
+library(rtweet)
+library(topicmodels)
+library(zoo)
+
+
+
 
 # Data Preparation ----
 
@@ -62,8 +80,7 @@ write.csv(hdaily,"hdaily.csv")
 
 # Rewriting Dates ---- 
 
-install.packages("zoo")
-library(zoo)
+
 
 
 guardian$Date
@@ -86,12 +103,10 @@ as.yearmon(guardian$Date[1])
 nyt2 <- nyt[order(nyt$Date),] 
 nyt2$MonthYear <- as.Date(nyt2$MonthYear)
 nyt2$Date <- as.Date(nyt2$Date)
-
 nyt2$Text <- as.character(nyt2$Text)
 
+
 ncorpus <- corpus(nyt2,text_field = "Text")
-
-
 head(docvars(ncorpus))
 
 
@@ -102,32 +117,19 @@ ncorp_docs <- corpus_reshape(ncorp_docs, to = "documents")
 ntok <- tokens(ncorp_docs, remove_punct = TRUE)
 ntok <- tokens_remove(ntok, pattern = stopwords('en'))
 ntok <- tokens_remove(ntok, pattern = c("Hong","Kong","KONG","HONG","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))
-
-
-
 context_keyword <- kwic(ntok, pattern = "elect*", window = 3)
 context_keywords <- kwic(ntok, pattern = c("elect*","victor*"), window = 3)
 context_pattern <- kwic(ntok, pattern = phrase('Junius Ho*'), window = 7)
-
-
 #Removal of tokens changes the lengths of documents, but they remain the same if you set padding = TRUE. This option is useful especially when you perform positional analysis.
-
 multiword <- kwic(ntok, pattern = phrase(c("Junius Ho","Joshua Wong")))
 head(multiword)
-
 #To preserve these expressions in bag-of-word analysis, you have to compound them using tokens_compound().
-
 toks_comp <- tokens_compound(ntok_nostop, pattern = phrase(c("Junius Ho","Joshua Wong")))
 kw_comp <- kwic(toks_comp, pattern = c("Junius_Ho","Joshua_Wong"))
 head(kw_comp, 10)
-
 #Can also generate ngrams
-
 toks_ngram <- tokens_ngrams(ntok_clean, n = 2)
 head(toks_ngram)
-
-
-
 # DFM ----
 
 nyt_dfm <- dfm(ntok)
@@ -201,9 +203,7 @@ plot(clust, xlab = "Distance", ylab = NULL)
 
 # Relative Frequency Analysis ----
 
-require(quanteda)
-require(quanteda.corpora)
-require(lubridate)
+
 
 tstat_key <- textstat_keyness(nyt_dfm, 
                               target = month(docvars(nyt_dfm, 'MonthYear')) >= "2019-09-01")
@@ -212,4 +212,71 @@ attr(tstat_key, 'documents') <- c('2019-07-01', '2019-08-01','2019-09-01')
 textplot_keyness(tstat_key)
 
 # Chinese Data ----
+
+hdaily2 <- hdaily[order(hdaily$Date),] 
+hdaily2$MonthYear <- as.Date(hdaily2$MonthYear)
+hdaily2$Date <- as.Date(hdaily2$Date)
+hdaily2$Text <- as.character(hdaily2$Text)
+
+CNcorpus <- corpus(hdaily2,text_field = "Text")
+
+#Stopwords
+CN_stop <- stopwords("zh", source = "misc")
+
+#Tokenize 
+CNtok <- CNcorpus %>% 
+  tokens(remove_punct = TRUE) %>%
+  tokens_remove(pattern = CN_stop) %>%
+  tokens_remove(pattern = c("指","香港","條例","會","時","逃犯","示威者","修訂",
+                            "香","港"
+  ))
+#DFM
+
+CN_dfm <- dfm(CNtok)
+topfeatures(CN_dfm)
+
+#Word Cloud
+
+# plot a word cloud
+set.seed(100)
+
+# to set the font correctly for macOS
+textplot_wordcloud(CN_dfm, min_count = 25, random_order = FALSE,
+                   rotation = .25, max_words = 200,
+                   min_size = 0.5, max_size = 2.8,
+                   font = if (Sys.info()['sysname'] == "Darwin") "SimHei" else NULL,
+                   color = RColorBrewer::brewer.pal(8, "Dark2"))
+
+
+
+# fcm within the window size of 5
+ch10_corp <- corpus_subset(CNcorpus, MonthYear > "2017-10-01")
+ch10_toks <- 
+  tokens(ch10_corp, remove_punct = TRUE) %>% 
+  tokens_remove(CN_stop)
+ch_fcm <- fcm(ch10_toks, context = "window")  
+topfeatures(ch_fcm["暴力", ])
+
+# collocations
+
+cn_col <- textstat_collocations(CNtok, size = 2:4, min_count = 20)
+knitr::kable(head(cn_col, 10))
+
+# bigrams since october
+ch10_col <- textstat_collocations(ch10_toks, size = 2)
+knitr::kable(head(ch10_col, 10))
+
+#Document Scaling?
+
+wf <- textmodel_wordfish(CN_dfm)
+y <- as.Date("2019-09-01"):as.Date("2019-12-01")
+y <- y[y <= "2019-10-01" | y >= "2019-11-01"]
+y <- y[!y %in% c("2019-10-01", "2019-10-15", "2019-10-31", "2019-11-01", "2019-11-15","2019-11-30")]
+plot(y, wf$theta, xlab = "Month", ylab = "Position")
+
+
+
+
+
+
 
